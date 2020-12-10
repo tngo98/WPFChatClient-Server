553d821f21aa3decc660de8c6b73a3ebd3a77a1f
Tommy Ngo <TommyKhoaNgo@live.com>
2020-12-10 13:24:37 -0500

    Add project files.

---
+++ ChatServer/Program.cs
@@ -0,0 +1,135 @@
+﻿//
+// FILE			:	Program.cs
+// PROJECT		:	Windows Programming - PROG2121 - Assignment #5
+// PROGRAMMER	:	Julian Lichty, Muhammad Mamooji
+// DESCRIPTION	:	This file contains methods for the server side logic for the chat box application
+//
+
+
+//Using Statements
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+//Main Program
+namespace ChatServer
+{
+    class Program
+    {
+        private static bool keepListening = true;
+        private static List<Thread> clientThreads = new List<Thread>();
+        private static List<TcpClient> clients = new List<TcpClient>();
+        private static List<NetworkStream> clientStreams = new List<NetworkStream>();
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        static void Main(string[] args)
+        {
+            //Set Server to NULL
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.0.10");
+                server = new TcpListener(localAddr, port);
+
+                //Start listening for client requests
+                server.Start();
+
+                //Enter the listening loop
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... \n");
+
+                    // Perform a blocking call to accept requests.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!\n");
+
+                    clients.Add(client);
+
+                    NetworkStream stream = client.GetStream();
+                    clientStreams.Add(stream);
+
+                    //Create a new thread to listen to the client
+                    Thread newThread = new Thread(new ParameterizedThreadStart(Worker));
+
+                    //Add the thread to the list and start the thread with the client's stream
+                    clientThreads.Add(newThread);
+                    newThread.Start(client);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}\n", e);
+            }
+            finally
+            {
+                //Stop listening for new clients
+                server.Stop();
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public static void Worker(Object o)
+        {
+            //Variables
+            TcpClient client = (TcpClient)o;
+            byte[] bytes = new byte[256];
+            string data = string.Empty;
+            NetworkStream stream = client.GetStream();
+            int i = 0;
+
+            while (keepListening)
+            {
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("\nReceived: {0}", data);
+
+                    if (data == "/[+ClientEnd+]/")
+                    {
+                        for (int s = 0; s < clientStreams.Count; s++)
+                        {
+                            if (clientStreams[s] == stream)
+                            {
+                                stream.Close();
+                                client.Close();
+                                clientThreads[s].Interrupt();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //Loop through clientStreams list
+                        for (int s = 0; s < clientStreams.Count; s++)
+                        {
+                            //check that the current element in clientStreams isn't the same stream used in this thread
+                            if (clientStreams[s] != stream)
+                            {
+                                //Convert the msg recieved from the client to bytes (with userID, according to the server, attached)
+                                byte[] msg = System.Text.Encoding.ASCII.GetBytes(("Client " + s + "/[+UserId+]/" + data));
+
+                                //Send the message to the other clients
+                                clientStreams[s].Write(msg, 0, msg.Length);
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    }
+}
---
+++ ChatWPF/MainWindow.xaml.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+
+namespace ChatWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //TCP Connection
+        private Int32 port = 13000;
+        private string server = "2607:fea8:1e60:eee:94e8:7dbd:c9c4:b63";
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread serverListener;
+        //Data members
+        private string[] userIdDelimiter = new string[] { "/[+UserId+]/" };
+        private bool MouseInLogs = false;
+        private bool Scrolling = false;
+        private bool continueListen = true;
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //
+        // METHOD		:	MainWindow()
+        // DESCRIPTION	:	This method initializes the main window, starts the server 
+        //                  and connects to the client
+        // PARAMETERS   :   none
+        // RETURNS		:	none
+        //
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += MainWindow_Close;
+
+            //Assigns the method to the thread
+            serverListener = new Thread(new ParameterizedThreadStart(ListenToServer));
+            //Set thread working flag to true
+            continueListen = true;
+            //Start the thread
+            serverListener.Start(stream);
+
+            try
+            {
+                //Connect TCP client to server
+                client = new TcpClient(server, port);
+                //Setup service stream with client
+                stream = client.GetStream();
+            }
+            catch (ArgumentNullException Ae)
+            {
+                MessageBox.Show(Ae.ToString());
+            }
+            catch (SocketException Se)
+            {
+                MessageBox.Show(Se.ToString());
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //
+        // METHOD		:	MainWindow_Close()
+        // DESCRIPTION	:	This method shuts down all the running functions and threads when the 
+        //                  user exits the program
+        // PARAMETERS   :   object sender       :   object that called the funtion
+        //                  CancelEventArgs e   :   any arguments if an event is canceled
+        // RETURNS		:	void
+        //
+        private void MainWindow_Close(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                //Set thread listener flag to false
+                continueListen = false;
+                //Close Stream 
+                stream.Close();
+                //Close Client
+                client.Close();
+            }
+            catch (ArgumentNullException ARe)
+            {
+                MessageBox.Show(ARe.ToString());
+            }
+            catch (SocketException SKe)
+            {
+                MessageBox.Show(SKe.ToString());
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //
+        // METHOD		:	Post_Message()
+        // DESCRIPTION	:	This method connects the client to the server and displays the client's 
+        //                  message in the chatbox
+        // PARAMETERS   :   string message  :   message sent by the client
+        //                  string userID   :   holds the user id 
+        // RETURNS		:	void
+        //
+        private void Post_Message(string message, string userID)
+        {
+            try
+            {
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Send the message to the connected TcpServer. 
+                if (stream != null)
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+                else
+                {
+                    MessageBox.Show("SERVER NOT ONLINE");
+                }
+
+                //Add the message to the chatlogs at the same time
+                AddToChatLog(message, userID);
+            }
+            catch (ArgumentNullException ANe)
+            {
+                MessageBox.Show(ANe.ToString());
+            }
+            catch (SocketException SCKe)
+            {
+                MessageBox.Show(SCKe.ToString());
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //
+        // METHOD		:	ListenToServer(object o)
+        // DESCRIPTION	:	This method waits for readible data from the stream and converts it to 
+        //                  a string and passes this to AddToChatLog function
+        // PARAMETERS   :   object o    :   object that called the function 
+        // RETURNS		:	void
+        //
+        private void ListenToServer(object o)
+        {
+            //Variables
+            Int32 bytes;
+            Byte[] data = new Byte[256];
+            String responseData = String.Empty;
+
+            while (continueListen)
+            {
+                //Reset variable values
+                responseData = string.Empty;
+                data = new byte[256];
+
+                if (stream != null)
+                {
+                    if (stream.CanRead == true && stream.DataAvailable == true)
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        //Convert bytes to string
+                        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                        //Parse the userID
+                        string[] serverResponse = responseData.Split(userIdDelimiter, StringSplitOptions.None);
+                        //Add the message to the chatbox
+                        AddToChatLog(serverResponse[1], serverResponse[0]);
+                    }
+                }
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //
+        // METHOD		:	AddToChatLog()
+        // DESCRIPTION	:	This method initializes the main window, starts the server 
+        //                  and connects to the client
+        // PARAMETERS   :   string inputMessage  :  message to be added to the chat box
+        //                  string senderID      :  the ID of the message sender
+        // RETURNS		:	void
+        //
+        private void AddToChatLog(string inputMessage, string senderID)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                TextBlock newTxtBlck = new TextBlock();
+                newTxtBlck.Text = inputMessage;
+
+                //Adds message block to the chatbox
+                chatBox.Items.Add(newTxtBlck);
+
+                //Scroll chatbox keep latest messages in view
+                if (!(MouseInLogs == true && Scrolling == true))
+                {
+                    chatBoxScroll.ScrollToEnd();
+                }
+            }));
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //
+        // METHOD		:	Return_Check()
+        // DESCRIPTION	:	This method checks if the user has pressed the enter button, then passes 
+        //                  the object to the appropriate event handler
+        // PARAMETERS   :   object sender   :   object that called the function 
+        //                  KeyEventArgs e  :   any arguments of key events
+        // RETURNS		:	void
+        //
+
+        private void messageText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                this.sendMessageButton_Click(sender, e);
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //
+        // METHOD		:	Send_Message()
+        // DESCRIPTION	:	This method checks if the message is empty then adds it to the WPF 
+        //                  chatbox and passes the massage to the appropriate event handler
+        // PARAMETERS   :   object sender       :   object that called the function
+        //                  RoutedEventArgs e   :   any arguments of routed events
+        // RETURNS		:	void
+        //
+        private void sendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (messageText.Text != "")
+            {
+                string message = messageText.Text;
+                Post_Message(message, "You");
+                messageText.Text = "";
+            }
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    }
+}
